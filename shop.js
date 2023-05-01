1c5844250bbcb2e8aa5bc73e19e3ef7928cd9af1
mma6368 <117923243+mma6368@users.noreply.github.com>
2023-05-01 14:42:17 -0400

    Add files via upload

---
+++ shop.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { PRODUCTS } from "../../products";
+import { Product } from "./product";
+import ProductIMG from '../../assets/product.png';
+import './shop.css';
+
+
+export const Shop = () => {
+  return (
+    <div className="shop"style={{ backgroundImage: `url(${ProductIMG})`}}>
+      <div className="cartTitle">
+        <p><br></br></p>
+      </div>
+      <div className="shopTitle">
+        <h1></h1>
+      </div>
+
+      <div className="products">
+        {PRODUCTS.map((product) => (
+          <Product data={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
